994b4c2a61b83d269f93e5a3e1ccb40d4d1cea1e
cherukuri333 <teja.cherukuri3@gmail.com>
2025-04-29 08:49:04 +0530

    added bundle for datbricks

---
+++ notebooks/my_notebook.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ## Basic Notebook Example
+# MAGIC This is a simple notebook to test Databricks bundle deployment.
+
+# COMMAND ----------
+
+# Print a welcome message
+print("Hello from Databricks!")
+
+# COMMAND ----------
+
+# Basic arithmetic
+a = 5
+b = 3
+sum_result = a + b
+print(f"{a} + {b} = {sum_result}")
